eb3b444ea6bb242991003e9ead9aa0c2458c6846
Ronier <ronier.cavalari@outlook.com>
2024-01-05 16:27:21 -0300

    página de categorias

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,21 @@ var btnPageSearch = document.getElementById("btnPageSearch");
 var pageAdd = document.getElementById("add-page");
 var pageSearch = document.getElementById("search-page");
 
-btnPageAdd.addEventListener("click", showAddPage);
-btnPageSearch.addEventListener("click", showSearchPage);
 
 var formTitle = document.getElementById("form-title");
 var formCategory = document.getElementById("form-category");
 var formAnnotations = document.getElementById("form-annotations");
 var formButton = document.getElementById("form-sub-button");
 
-//Ao clicar no botão, salvar como Favorito
-formButton.addEventListener("click", saveFav);
+
+formButton.addEventListener("click", saveFav); //Ao clicar no botão, salvar como Favorito
+btnPageAdd.addEventListener("click", showAddPage);
+btnPageSearch.addEventListener("click", showSearchPage);
 
 var urlCurrent = "";
 var urlIconCurrent = "";
+
+var listCategory = [];
 
 
 window.onload = function () {
@@ -56,18 +58,16 @@ function saveFav() {
     }
 
     var newJson = JSON.stringify(dict);
-    chrome.storage.sync.get(["STORAGE_KEY"]).then((result) => {
+    chrome.storage.sync.get("STORAGE_KEY", function(result){ //Função que retorna as info salva em storage
         if(result.STORAGE_KEY != undefined){
-            newJson = result.STORAGE_KEY + "\n" + newJson;
+            newJson = result["STORAGE_KEY"] + "\n" + newJson;
         }
+        chrome.storage.sync.set({ "STORAGE_KEY": newJson }).then(() => { //Função que salva em storage
+            console.log("Salvo com sucesso");
+            getFavs();
+            showSearchPage();
+        });
     });
-
-    chrome.storage.sync.set({ "STORAGE_KEY": newJson }).then(() => {
-        console.log("Salvo com sucesso");
-        getFavs();
-        showSearchPage();
-    });
-
 }
 
 
@@ -75,7 +75,54 @@ function saveFav() {
 function getFavs() {
     var divCategoryContainer = document.getElementById("category-container");
 
-    chrome.storage.sync.get(["STORAGE_KEY"]).then((favs) => {
-        divCategoryContainer.innerHTML += favs.STORAGE_KEY;
+    chrome.storage.sync.get("STORAGE_KEY", function(result){ //Função que retorna as info salva em storage
+        var tempListFavs = result.STORAGE_KEY.split("\n");
+
+        var listFavs = [];
+        for(var i = 0; i < tempListFavs.length; i++){
+            let element = JSON.parse(tempListFavs[i]);
+            let category = element["category"]; 
+
+            listFavs.push(element);
+            if(!listCategory.includes(category)){ // se na lista de categorias não tiver a lista atual
+                listCategory.push(category);
+            }
+        }
+
+        populateSearchPage();
+        //divCategoryContainer.innerHTML = listCategory;
     });
+}
+
+function populateSearchPage(){
+    var divCategoryContainer = document.getElementById("category-container");
+    divCategoryContainer.innerHTML = "";
+    console.log("aq");
+
+    
+    for(i = 0; i < listCategory.length; i++){
+        let categoryName = listCategory[i];
+
+        var categoryDiv = document.createElement("div");
+        categoryDiv.id = "category" + i.toString();
+        categoryDiv.classList.add("col-4");
+
+        categoryDiv.innerHTML = `
+            <div class="category-header">
+                <img src="assets/folder.png" height="25" />
+            </div>
+            <div class="category-body">
+                ${categoryName}
+            </div>
+        `;
+        divCategoryContainer.appendChild(categoryDiv);
+
+        document.getElementById("category" + i.toString()).addEventListener("click", () => {
+            showListByCategory(categoryName)
+        })
+    }
+}
+
+function showListByCategory(categoryName){
+    console.log(categoryName);
 }
