8387f5e5e7e374c8de447a175f7c361baa2112c9
Ronier <ronier.cavalari@outlook.com>
2024-01-08 13:28:29 -0300

    pagina de links | first release

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ var btnPageSearch = document.getElementById("btnPageSearch");
 
 var pageAdd = document.getElementById("add-page");
 var pageSearch = document.getElementById("search-page");
+var pageLinks = document.getElementById("links-page");
 
 
 var formTitle = document.getElementById("form-title");
@@ -18,6 +19,7 @@ btnPageSearch.addEventListener("click", showSearchPage);
 var urlCurrent = "";
 var urlIconCurrent = "";
 
+var listFavs = [];
 var listCategory = [];
 
 
@@ -39,11 +41,21 @@ window.onload = function () {
 function showAddPage() {
     pageAdd.style.display = "block";
     pageSearch.style.display = "none";
+    pageLinks.style.display = "none";
 }
 function showSearchPage() {
     getFavs(); //Carregar favoritos.
     pageAdd.style.display = "none";
-    pageSearch.style.display = "block";
+    pageSearch.style.display = "block";    
+    pageLinks.style.display = "none";
+}
+// Pagina de links
+function showLinksPage(categoryName) {
+    pageAdd.style.display = "none";
+    pageSearch.style.display = "none";
+    document.getElementById("link-catg-name").innerHTML = categoryName;
+    pageLinks.style.display = "block";
+
 }
 
 //Função que salva como Favorito
@@ -73,12 +85,12 @@ function saveFav() {
 
 //Carregar favoritos.
 function getFavs() {
+    listFavs = [];
     var divCategoryContainer = document.getElementById("category-container");
 
     chrome.storage.sync.get("STORAGE_KEY", function(result){ //Função que retorna as info salva em storage
         var tempListFavs = result.STORAGE_KEY.split("\n");
 
-        var listFavs = [];
         for(var i = 0; i < tempListFavs.length; i++){
             let element = JSON.parse(tempListFavs[i]);
             let category = element["category"]; 
@@ -97,7 +109,6 @@ function getFavs() {
 function populateSearchPage(){
     var divCategoryContainer = document.getElementById("category-container");
     divCategoryContainer.innerHTML = "";
-    console.log("aq");
 
     
     for(i = 0; i < listCategory.length; i++){
@@ -124,5 +135,60 @@ function populateSearchPage(){
 }
 
 function showListByCategory(categoryName){
-    console.log(categoryName);
+    populateLinkPage(categoryName);
+    showLinksPage(categoryName);
+}
+
+function populateLinkPage(categoryName){
+    var divLinksContainer = document.getElementById("links-container");
+    divLinksContainer.innerHTML = "";
+    console.log("aq");
+
+    for(var i = 0; i < listFavs.length; i++){
+        var fav = listFavs[i];
+        if (fav["category"] == categoryName){
+            //divLinksContainer.innerHTML += fav["url"];
+            var linkDiv = document.createElement("div");
+            linkDiv.id = "fav" + i.toString();
+
+            linkDiv.classList.add("row");
+            linkDiv.classList.add("justify-content-center");
+
+            linkDiv.innerHTML = `
+            <div class="row justify-content-center">
+                <div class="link-header col-1 center">
+                    <img src="${fav["iconUrl"]}" height="20" />
+                </div>
+                <div class="link-body col-11">
+                    <div class="row align-itens-center">
+                        <div class="col-10">
+                            <div class="row link-title">
+                                ${fav["title"]}
+                            </div>
+                            <div class="row">
+                                ${fav["annotations"]}
+                            </div>
+                        </div>
+                        <div class="col-2 center">
+                            <div class="row align-itens-center">
+                                <div class="col-auto">
+                                    <a href="${fav["url"]}" target="_blank" rel="noopener">
+                                        <img src="assets/link.png" height="20" />
+                                    </a>
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-nopadding">
+                                        <img src="assets/delete.png" height="20" />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            divLinksContainer.appendChild(linkDiv);
+        }
+    }
 }
